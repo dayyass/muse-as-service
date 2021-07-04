a532619ddbc8ce80fd483bcb35ec0a9b9ec3eddb
Dani El-Ayyass <dayyass@yandex.ru>
2021-07-04 08:37:01 +0300

    update gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,7 +13,15 @@ def max_workers_and_threads() -> int:
     return multiprocessing.cpu_count() * 2 + 1
 
 
-bind = "0.0.0.0:5000"
-workers = max_workers_and_threads()
-threads = max_workers_and_threads()
-timeout = 0
+HOST = "0.0.0.0"
+PORT = 5000
+
+
+# GUNICORN OPTIONS
+
+bind = f"{HOST}:{PORT}"  # The socket to bind
+workers = max_workers_and_threads()  # The number of worker processes for handling requests
+threads = max_workers_and_threads()  # The number of worker threads for handling requests
+timeout = 0  # Workers silent for more than this many seconds are killed and restarted
+
+# You can also add other gunicorn options
