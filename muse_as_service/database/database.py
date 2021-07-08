64788215678eff0576f0a60c4a2f53ff69538569
Dani El-Ayyass <dayyass@yandex.ru>
2021-07-08 13:13:46 +0300

    fix RevokedTokenModel

--- muse_as_service/database/database.py
+++ muse_as_service/database/database.py
@@ -51,6 +51,10 @@ class RevokedTokenModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     jti = db.Column(db.String(120))
 
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def is_jti_blocklisted(cls, jti: str) -> bool:
         query = cls.query.filter_by(jti=jti).first()
