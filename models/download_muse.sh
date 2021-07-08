d099d9e46793e4a741fa8a87c557ee253f080c12
Dani El-Ayyass <dayyass@yandex.ru>
2021-07-09 00:25:50 +0300

    update ci to wotk with windows

--- models/download_muse.sh
+++ models/download_muse.sh
@@ -3,7 +3,7 @@
 DOWNLOAD_LINK="https://tfhub.dev/google/universal-sentence-encoder-multilingual/3?tf-hub-format=compressed"
 MODEL_NAME="universal-sentence-encoder-multilingual_3"
 
-wget $DOWNLOAD_LINK -O models/${MODEL_NAME}.tar.gz
+curl $DOWNLOAD_LINK -L -o models/${MODEL_NAME}.tar.gz
 mkdir models/${MODEL_NAME}
 tar xvzf models/${MODEL_NAME}.tar.gz -C models/${MODEL_NAME}
 rm models/${MODEL_NAME}.tar.gz
